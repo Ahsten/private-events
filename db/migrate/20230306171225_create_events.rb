c81cf3589270212b50f23b92a0f28198bcd39830
Luxe17 <hanson1717@live.com>
2023-03-06 10:21:38 -0700

    Generated event model

---
+++ db/migrate/20230306171225_create_events.rb
@@ -0,0 +1,15 @@
+class CreateEvents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.text :description
+      t.string :location
+      t.date :start_date
+      t.date :end_date
+      t.time :start_time
+      t.time :end_time
+
+      t.timestamps
+    end
+  end
+end
