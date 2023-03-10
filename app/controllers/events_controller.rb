9ff131dcb62d5517b9efe8a0c789ac0c0de923a2
Luxe17 <hanson1717@live.com>
2023-03-10 10:33:52 -0600

    Added show to display event details

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,5 +2,33 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    def new
+        @even = Event.new
+    end
     
+    def create
+        @event = current_user.created_events.build(event_params)
+
+        if @event.save
+            redirect_to @event
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def event_params
+        params.require(:event).permit(:creator_id, 
+                                        :title, 
+                                        :start_date, 
+                                        :end_date, 
+                                        :start_time, 
+                                        :end_time, 
+                                        :locaiton, 
+                                        :description)
+    end
 end
